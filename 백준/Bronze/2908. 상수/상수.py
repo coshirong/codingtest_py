17a0b739204242db28b7c319f954e23f850916ba
coshirong <hyangee53@gmail.com>
2026-03-12 21:43:48 +0900

    [Bronze II] Title: 상수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,3 +1,5 @@
-a, b = map(str, input().split())
-res = a[::-1] if a[::-1] > b[::-1] else b[::-1]
-print(res)
+import sys
+input = sys.stdin.readline
+a, b = input().split()
+max_val = a[::-1] if int(a[::-1]) > int(b[::-1]) else b[::-1]
+print(max_val)
